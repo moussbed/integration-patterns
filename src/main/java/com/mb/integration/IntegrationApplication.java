23d88c3434af9cd2775c8c271da88badcd02fbe0
moussbed bedril <moussakatbedril@yahoo.com>
2025-01-27 16:41:15 +0100

    building integration manually

--- src/main/java/com/mb/integration/IntegrationApplication.java
+++ src/main/java/com/mb/integration/IntegrationApplication.java
@@ -1,18 +1,20 @@
 package com.mb.integration;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.integration.core.GenericHandler;
-import org.springframework.integration.core.GenericSelector;
 import org.springframework.integration.core.GenericTransformer;
 import org.springframework.integration.core.MessageSource;
 import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.context.IntegrationFlowContext;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
 
 import java.time.Instant;
+import java.util.Set;
 
 @SpringBootApplication
 public class IntegrationApplication {
@@ -24,7 +26,7 @@ public class IntegrationApplication {
 	private static String text(){
 		return Math.random()> .5 ?
 				String.format("Hello World @  %s !", Instant.now()) :
-				String.format("hola todo el mundo @ %s !", Instant.now());
+				String.format("Hola todo el mundo @ %s !", Instant.now());
 	}
 
 	@Component
@@ -35,20 +37,32 @@ public class IntegrationApplication {
 		}
 	}
 
-	@Bean
-	IntegrationFlow flow(MyMessageSource messageSource){
+
+	private static IntegrationFlow buildIntegrationFlow(MyMessageSource messageSource, int milliseconds, String filterText, String name){
 		return IntegrationFlow
 				.from(messageSource,
 						sourcePollingChannelAdapterSpec ->
 								sourcePollingChannelAdapterSpec.poller(
-										pollerFactory -> pollerFactory.fixedRate(2000)))
-				.filter(String.class, source -> source.contains("hola"))
+										pollerFactory -> pollerFactory.fixedRate(milliseconds)))
+				.filter(String.class, source -> source.contains(filterText))
 				.transform((GenericTransformer<String, String>) String::toUpperCase)
 				.handle((GenericHandler<String>) (payload, headers) -> {
-					System.out.printf("The payload is %s%n", payload);
+					System.out.printf("[%s] The payload is %s%n",name, payload);
 					return null;
 				})
 				.get();
+	}
+
+	@Bean
+	ApplicationRunner runner (MyMessageSource messageSource, IntegrationFlowContext context){
+		return args -> {
+			Set<IntegrationFlow> integrationFlows = Set.of(buildIntegrationFlow(messageSource, 1000, "Hola", "Flow-00"),
+					buildIntegrationFlow(messageSource, 3000, "Hello","Flow-01" ));
+
+			integrationFlows.forEach(integrationFlow ->
+					context.registration(integrationFlow).register().start());
+
+		};
 	}
 
 
