312866cefb7e2e89441255d96c59bf85fcda3919
moussbed bedril <moussakatbedril@yahoo.com>
2025-03-18 11:48:20 +0100

    database integration

--- src/main/java/com/mb/integration/IntegrationApplication.java
+++ src/main/java/com/mb/integration/IntegrationApplication.java
@@ -1,27 +1,24 @@
 package com.mb.integration;
 
-import org.apache.commons.net.ftp.FTPFile;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.integration.annotation.IntegrationComponentScan;
 import org.springframework.integration.core.GenericHandler;
-import org.springframework.integration.core.GenericSelector;
 import org.springframework.integration.dsl.IntegrationFlow;
-import org.springframework.integration.dsl.PollerFactory;
-import org.springframework.integration.file.FileReadingMessageSource;
-import org.springframework.integration.file.FileWritingMessageHandler;
-import org.springframework.integration.file.dsl.Files;
-import org.springframework.integration.file.remote.session.SessionFactory;
-import org.springframework.integration.file.support.FileExistsMode;
-import org.springframework.integration.ftp.dsl.Ftp;
-import org.springframework.integration.ftp.inbound.FtpInboundFileSynchronizingMessageSource;
-import org.springframework.integration.ftp.session.DefaultFtpSessionFactory;
+import org.springframework.integration.jdbc.JdbcPollingChannelAdapter;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.messaging.MessageHeaders;
+import org.springframework.stereotype.Component;
 
-import java.io.File;
-import java.time.Duration;
+import javax.sql.DataSource;
+import java.math.BigDecimal;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
 import java.util.logging.Logger;
 
 
@@ -35,76 +32,76 @@ public class IntegrationApplication {
 		Thread.currentThread().join();
     }
 
-//    @Bean
-//    IntegrationFlow inboundFileFlow(@Value("${HOME}/Desktop/in") File in,
-//                                    @Value("${HOME}/Desktop/out") File out) {
-//        FileReadingMessageSource inboundFileAdapter = Files.inboundAdapter(in)
-//                .autoCreateDirectory(true)
-//                .recursive(true)
-//                .getObject();
-//        FileWritingMessageHandler outboundFileAdapter = Files.outboundAdapter(out)
-//                .autoCreateDirectory(true)
-//                .fileNameGenerator(message -> Long.toString(System.currentTimeMillis()))
-//                .fileExistsMode(FileExistsMode.FAIL)
-//                .deleteSourceFiles(true)
-//                .getObject();
-//        return IntegrationFlow
-//                .from(inboundFileAdapter, c -> c.poller(p -> PollerFactory.fixedRate(Duration.ofSeconds(1))))
-//                .filter(File.class, source -> source.isFile() && source.getName().endsWith(".csv"))
-//                .handle(new GenericHandler<File>() {
-//                    @Override
-//                    public Object handle(File payload, MessageHeaders headers) {
-//                        log.info(String.format("Received: %s", payload.getAbsolutePath()));
-//                        headers.forEach((k, v) -> log.info(String.format("%s: %s", k, v)));
-//                        return payload;
-//                    }
-//                })
-//                .handle(outboundFileAdapter)
-//                .get();
-//    }
-
     @Bean
-    SessionFactory<FTPFile> sessionFactory() {
-        DefaultFtpSessionFactory ftpSessionFactory = new DefaultFtpSessionFactory();
-        ftpSessionFactory.setHost("localhost");
-        ftpSessionFactory.setPort(21);
-        ftpSessionFactory.setUsername("bedril");
-        ftpSessionFactory.setPassword("1234");
-        return ftpSessionFactory;
+    JdbcPollingChannelAdapter jdbcPollingChannelAdapter(DataSource dataSource, AccountStatementRowMapper accountStatementRowMapper) {
+        var jdbcPollingChannelAdapter = new JdbcPollingChannelAdapter(dataSource, "select * from account_statement where balance IS NOT NULL");
+        jdbcPollingChannelAdapter.setRowMapper(accountStatementRowMapper);
+        jdbcPollingChannelAdapter.setUpdateSql("update account_statement set status = 'MODIFIED'  where id= :id ");
+        jdbcPollingChannelAdapter.setUpdatePerRow(true);
+        jdbcPollingChannelAdapter.setUpdateSqlParameterSourceFactory(input -> {
+            if(input instanceof AccountStatement accountStatement) {
+                return new MapSqlParameterSource("id", accountStatement.id());
+            }
+            return null;
+        });
+        return jdbcPollingChannelAdapter;
     }
-
     @Bean
-    IntegrationFlow inboundFtpFlow(
-            @Value("${HOME}/Desktop/files/local") File ftpLocal,
-            SessionFactory<FTPFile> ftpSessionFactory
-    ) {
-        FtpInboundFileSynchronizingMessageSource ftpMessageSource = Ftp.inboundAdapter(ftpSessionFactory)
-                .autoCreateLocalDirectory(true)
-                .localDirectory(ftpLocal)
-                .deleteRemoteFiles(true)
-                .getObject();
-
-        var outboundFtpAdapter = Ftp.outboundAdapter(ftpSessionFactory)
-                .autoCreateDirectory(true)
-                .temporaryRemoteDirectory("temp")
-                .remoteDirectory("processed")
-                .fileNameGenerator(message -> Long.toString(System.currentTimeMillis()))
-                .getObject();
-
-        return IntegrationFlow
-                .from(ftpMessageSource)
-                .filter(File.class, source -> source.isFile() && source.getName().endsWith(".csv"))
-                .handle(new GenericHandler<File>() {
+    IntegrationFlow integrationFlow(JdbcPollingChannelAdapter jdbcPollingChannelAdapter){
+        return IntegrationFlow.from(jdbcPollingChannelAdapter, poller-> poller.poller(pm->pm.fixedRate(1000)))
+                .handle(new GenericHandler<List<AccountStatement>>() {
                     @Override
-                    public Object handle(File payload, MessageHeaders headers) {
-                        log.info(String.format("Received: %s", payload.getAbsolutePath()));
-                        headers.forEach((k, v) -> log.info(String.format("%s: %s", k, v)));
-                        return payload;
+                    public Object handle(List<AccountStatement> accountStatementList, MessageHeaders headers) {
+                        for (AccountStatement payload : accountStatementList) {
+                            log.info("*".repeat(50));
+                            log.info(payload.toString());
+                        }
+                        return null;
                     }
                 })
-                .handle(outboundFtpAdapter)
                 .get();
     }
 
-
 }
+
+@Component
+class AccountStatementRowMapper  implements RowMapper<AccountStatement>{
+
+    @Override
+    public AccountStatement mapRow(ResultSet rs, int rowNum) throws SQLException {
+        return new AccountStatement(
+                UUID.fromString(rs.getString("id")),
+                rs.getString("account_number"),
+                AccountOperation.valueOf(rs.getString("account_operation")),
+                rs.getBigDecimal("amount"),
+                rs.getBigDecimal("balance"),
+                rs.getObject("created_date", LocalDateTime.class),
+                rs.getObject("modified_date", LocalDateTime.class),
+                StatementStatus.valueOf(rs.getString("statement_status")),
+                Status.valueOf(rs.getString("status"))
+        );
+    }
+}
+
+record AccountStatement(UUID id,
+                        String accountNumber,
+                        AccountOperation accountOperation,
+                        BigDecimal amount,
+                        BigDecimal balance,
+                        LocalDateTime createdDate,
+                        LocalDateTime modifiedDate,
+                        StatementStatus statementStatus,
+                        Status status
+) {}
+
+enum StatementStatus {
+    ACCEPTED, PENDING, FAILED
+}
+enum Status {
+    CREATED, MODIFIED
+}
+enum AccountOperation {
+    DEBIT, CREDIT
+}
+
+
